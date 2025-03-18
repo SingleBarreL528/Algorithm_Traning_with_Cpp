ad052042de138de3b9cf2c9c65d3c357cba0f4cb
SeongMin Hong <117250328+SingleBarreL528@users.noreply.github.com>
2025-03-18 13:02:08 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 36 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
@@ -11,13 +11,12 @@ int main()
 
     int n, m;
     cin >> n >> m;
-    int arr[n + 1] = {0};
     int psum[n + 1] = {0};
 
     for (int idx = 1; idx <= n; ++idx)
     {
-        cin >> arr[idx];
-        psum[idx] = psum[idx - 1] + arr[idx];
+        cin >> psum[idx];
+        psum[idx] += psum[idx - 1];
     }
 
     while (m--)
@@ -25,8 +24,7 @@ int main()
         int i, j;
         cin >> i >> j;
 
-        int sum = psum[j] - psum[i - 1];
-        cout << sum << "\n";
+        cout << (psum[j] - psum[i - 1]) << "\n";
     }
 
     return 0;
