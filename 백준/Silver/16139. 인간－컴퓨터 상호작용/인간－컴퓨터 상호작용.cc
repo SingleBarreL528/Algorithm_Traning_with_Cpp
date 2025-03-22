aea98ff4974ced66e547d13af577ba0f5d577b5b
SeongMin Hong <117250328+SingleBarreL528@users.noreply.github.com>
2025-03-22 16:14:32 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 92 ms, Memory: 22844 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.cc
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.cc
@@ -4,29 +4,27 @@
 
 using namespace std;
 
+string s;
+int q;
+char a;
+int l, r;
+int arr[26][200001];
+
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    string s;
-    int q;
-    char a;
-    int l, r;
-    int arr[26][2000];
-
     cin >> s;
 
     for (int i = 0; i < 26; ++i)
     {
-        for (int j = 0; j < s.length(); ++j)
+        arr[i][0] = 0;
+        for (int j = 1; j <= s.length(); ++j)
         {
-            if (j > 0)
-            {
-                arr[i][j] = arr[i][j - 1];
-            }
-            if (s.at(j) == 'a' + i)
+            arr[i][j] = arr[i][j - 1];
+            if (s.at(j - 1) == 'a' + i)
             {
                 ++arr[i][j];
             }
@@ -38,7 +36,7 @@ int main()
     for (int i = 0; i < q; ++i)
     {
         cin >> a >> l >> r;
-        cout << arr[a - 'a'][r] - arr[a - 'a'][l - 1] << "\n";
+        cout << arr[a - 'a'][r + 1] - arr[a - 'a'][l] << "\n";
     }
 
     return 0;
