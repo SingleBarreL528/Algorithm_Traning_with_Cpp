70713a523eafa577717862561a093e16e3910db7
SeongMin Hong <117250328+SingleBarreL528@users.noreply.github.com>
2025-05-11 15:00:29 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 80 ms, Memory: 17448 KB -BaekjoonHub

--- 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.cc
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.cc
@@ -6,24 +6,18 @@
 
 using namespace std;
 
-bool comp(pair<string, int> &a, pair<string, int> &b)
+struct word_st
 {
-    if (a.second != b.second)
+    int cnt;
+    string s;
+
+    bool operator<(const word_st &other) const
     {
-        return a.second >= b.second;
+        if (cnt != other.cnt) return cnt > other.cnt;
+        if (s.length() != other.s.length()) return s.length() > other.s.length();
+        return s < other.s;
     }
-    else
-    {
-        if (a.first.length() != b.first.length())
-        {
-            return a.first.length() > b.first.length();
-        }
-        else
-        {
-            return a.first < b.first;
-        }
-    }
-}
+};
 
 int main()
 {
@@ -32,25 +26,32 @@ int main()
     cout.tie(NULL);
 
     int n, m;
-    string word;
-    unordered_map<string, int> word_list_map;
+    string str;
+    unordered_map<string, int> word_frep;
 
     cin >> n >> m;
     for (int i = 0; i < n; ++i)
     {
-        cin >> word;
-        if (word.length() >= m)
+        cin >> str;
+        if (str.length() >= m)
         {
-            word_list_map[word]++;
+            word_frep[str]++;
         }
     }
 
-    vector<pair<string, int>> word_list_vec(word_list_map.begin(), word_list_map.end());
-    sort(word_list_vec.begin(), word_list_vec.end(), comp);
-
-    for (auto iter = word_list_vec.begin(); iter != word_list_vec.end(); ++iter)
+    vector<word_st> word_list;
+    for (auto i : word_frep)
     {
-        cout << (*iter).first << "\n";
+        word_st word;
+        word.s = i.first;
+        word.cnt = i.second;
+        word_list.push_back(word);
+    }
+    sort(word_list.begin(), word_list.end());
+
+    for (auto i : word_list)
+    {
+        cout << i.s << "\n";
     }
 
     return 0;
